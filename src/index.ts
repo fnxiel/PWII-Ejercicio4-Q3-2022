4125e63008ab452b07ee32f45df1e8fbba4e46ef
fnxiel <fnxiel@unitec.edu>
2022-08-05 20:52:04 -0600

    Avance del ejercicio typescript

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function saludar(nombre: string, apellido: string, edad: number): void{
+    console.log("El nombre de esta persona es", nombre)
+    console.log("Apellido", apellido)
+    console.log("Edad",edad)
+}
+
+saludar("Luke", "Skywalker", 24)
---
+++ src/peliculas.ts
@@ -0,0 +1,11 @@
+type Pelicula = {
+    titulo: string
+    descripcion: string
+}
+
+let pelicula: Pelicula = {
+    titulo: "Bullet train",
+    descripcion: "Five assassins aboard a fast moving bullet train find out their missions have something in common."
+}
+
+console.log(pelicula)
